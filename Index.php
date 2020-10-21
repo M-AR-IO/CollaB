0cd347ecf8b58cf9b59c5040de5113d813b7720a
MARIO <71626084+M-AR-IO@users.noreply.github.com>
2020-10-21 13:19:17 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
+echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
+---------------------ini cobaan------------------
+echo "<h3>MARIO(195410194)</h3>"
+?>
