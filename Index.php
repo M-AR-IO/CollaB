4f80c2dd41d1c925272cc6c1dd06005686b4a8c8
MARIO <71626084+M-AR-IO@users.noreply.github.com>
2020-10-21 13:44:58 +0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,5 +2,4 @@
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
 echo "<h3>MARIO(195410194)</h3>"
-echo "<h3>Muhammad Ahyaruddin(195410186)</h3>"
 ?>
